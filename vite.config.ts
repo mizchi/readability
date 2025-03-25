3d63db9b3c0badaf67fb0d9885ee67acaa2047b6
mizchi <miz404@gmail.com>
2025-03-25 23:59:54 +0900

    for publish

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts'
 
 export default defineConfig({
+  plugins: [
+    dts({
+      include: 'v3/**/*.ts',
+      exclude: ['v3/**/*.test.ts', 'v3/test/**', 'v3/examples/**'],
+
+    }),
+  ],
   build: {
+    outDir: 'dist',
+    lib: {
+      entry: 'v3/index.ts',
+      name: 'Readability',
+      formats: ['es'],
+      fileName: () => 'dist.js',
+    },
     target: 'esnext',
+    sourcemap: true,
+    minify: 'esbuild',
   },
 });
